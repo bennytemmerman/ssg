945d966de3ad0e36a390d721b806a8136b5c17fe
bennytemmerman <81377775+bennytemmerman@users.noreply.github.com>
2025-03-16 21:20:28 +0100

    Update textnode.py

--- src/textnode.py
+++ src/textnode.py
@@ -1,29 +1,31 @@
-from enum import Enum
 from htmlnode import LeafNode
+from enum import Enum
+
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
-    PLAIN = "plain"
-    TEXT = "text"
+
 
 class TextNode:
-    def __init__(self, text: str, text_type: TextType, url: str = None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
-        if not isinstance(other, TextNode):
-            return False
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
 
     def __repr__(self):
-        return f"TextNode({self.text!r}, {self.text_type.value}, {self.url!r})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
 
 def text_node_to_html_node(text_node):
